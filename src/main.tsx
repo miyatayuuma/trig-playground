4376fa23f08557d81ddef79e64ab420f078db672
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2026-08-25 05:25:07 +0900

    feat: mount unit circle trace gateway

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LabApp from './LabApp'
+import UnitCircleGatewayPortal from './UnitCircleGatewayPortal'
 import VectorAdditionPortal from './VectorAdditionPortal'
+import { installMotionClock } from './motionClock'
 import './styles.css'
 import './labyrinth.css'
 import './vector-addition.css'
+import './unit-circle-gateway.css'
+
+installMotionClock()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LabApp />
+    <UnitCircleGatewayPortal />
     <VectorAdditionPortal />
   </React.StrictMode>,
 )
