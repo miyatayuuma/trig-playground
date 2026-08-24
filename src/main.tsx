fedef1f6aa3454bcfdae60a699067b5a4fb50c7e
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2026-08-25 00:04:35 +0900

    style: load vector addition hints

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import LabApp from './LabApp'
 import VectorAdditionPortal from './VectorAdditionPortal'
 import './styles.css'
 import './labyrinth.css'
+import './vector-addition.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
