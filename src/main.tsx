013d5029bbc61dc39313c0dbcccbc3d4260e6d23
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2026-08-25 00:02:35 +0900

    feat: mount vector addition extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LabApp from './LabApp'
+import VectorAdditionPortal from './VectorAdditionPortal'
 import './styles.css'
 import './labyrinth.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LabApp />
+    <VectorAdditionPortal />
   </React.StrictMode>,
 )
